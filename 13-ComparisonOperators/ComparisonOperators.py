19a7a704087358bc467ed9bd44823a1e313f281d
alexmaclean23 <alexmaclean23@gmail.com>
2019-11-20 23:34:27 -0500

    Add ComparisonOperators content

---
+++ 13-ComparisonOperators/ComparisonOperators.py
@@ -0,0 +1,440 @@
+# Declaration of a boolean variable
+checkValidity = True
+
+print()
+print("####################################################")
+print()
+
+# Equality comparison of integers
+checkValidity = (0 == 0)
+print(checkValidity)
+# True
+
+# Equality comparison of integers
+checkValidity = (1 == 0)
+print(checkValidity)
+# False
+
+# Equality comparison of floats
+checkValidity = (0.0 == 0.0)
+print(checkValidity)
+# True
+
+# Equality comparison of floats
+checkValidity = (1.0 == 0.0)
+print(checkValidity)
+# False
+
+# Equality comparison of Strings
+checkValidity = ("Zero" == "Zero")
+print(checkValidity)
+# True
+
+# Equality comparison of Strings
+checkValidity = ("Zero" == "One")
+print(checkValidity)
+# False
+
+# Equality comparison of Strings
+checkValidity = ("Zero" == "zero")
+print(checkValidity)
+# False
+
+# Equality comparison of characters
+checkValidity = ('a' == 'a')
+print(checkValidity)
+# True
+
+# Equality comparison of characters
+checkValidity = ('a' == 'b')
+print(checkValidity)
+# False
+
+# Equality comparison of characters
+checkValidity = ('a' == 'A')
+print(checkValidity)
+# False
+
+# Equality comparison of booleans
+checkValidity = (True == True)
+print(checkValidity)
+# True
+
+# Equality comparison of booleans
+checkValidity = (False == False)
+print(checkValidity)
+# True
+
+# Equality comparison of booleans
+checkValidity = (True == False)
+print(checkValidity)
+# False
+
+print()
+print("####################################################")
+print()
+
+# Inequality comparison of integers
+checkValidity = (0 != 0)
+print(checkValidity)
+# False
+
+# Inequality comparison of integers
+checkValidity = (1 != 0)
+print(checkValidity)
+# True
+
+# Inequality comparison of floats
+checkValidity = (0.0 != 0.0)
+print(checkValidity)
+# False
+
+# Inequality comparison of floats
+checkValidity = (1.0 != 0.0)
+print(checkValidity)
+# True
+
+# Inequality comparison of Strings
+checkValidity = ("Zero" != "Zero")
+print(checkValidity)
+# False
+
+# Inequality comparison of Strings
+checkValidity = ("Zero" != "One")
+print(checkValidity)
+# True
+
+# Inequality comparison of Strings
+checkValidity = ("Zero" != "zero")
+print(checkValidity)
+# True
+
+# Inquality comparison of characters
+checkValidity = ('a' != 'a')
+print(checkValidity)
+# False
+
+# Inequality comparison of characters
+checkValidity = ('a' != 'b')
+print(checkValidity)
+# True
+
+# Inequality comparison of characters
+checkValidity = ('a' != 'A')
+print(checkValidity)
+# True
+
+# Inequality comparison of booleans
+checkValidity = (True != True)
+print(checkValidity)
+# False
+
+# Inequality comparison of booleans
+checkValidity = (False != False)
+print(checkValidity)
+# False
+
+# Inequality comparison of booleans
+checkValidity = (True != False)
+print(checkValidity)
+# True
+
+print()
+print("####################################################")
+print()
+
+# Greater Than comparison of integers
+checkValidity = (1 > 0)
+print(checkValidity)
+# True
+
+# Greater Than comparison of integers
+checkValidity = (1 > 1)
+print(checkValidity)
+# False
+
+# Greater Than comparison of integers
+checkValidity = (1 > 2)
+print(checkValidity)
+# False
+
+# Greater Than comparison of floats
+checkValidity = (1.0 > 0.0)
+print(checkValidity)
+# True
+
+# Greater Than comparison of floats
+checkValidity = (1.0 > 1.0)
+print(checkValidity)
+# False
+
+# Greater Than comparison of floats
+checkValidity = (1.0 > 2.0)
+print(checkValidity)
+# False
+
+# Greater Than comparison of Strings
+checkValidity = ("B" > "A")
+print(checkValidity)
+# True
+
+# Greater Than comparison of Strings
+checkValidity = ("B" > "B")
+print(checkValidity)
+# False
+
+# Greater Than comparison of Strings
+checkValidity = ("B" > "C")
+print(checkValidity)
+# False
+
+# Greater Than comparison of Strings
+checkValidity = ("Z" > "z")
+print(checkValidity)
+# False
+
+# Greater Than comparison of characters
+checkValidity = ('B' > 'A')
+print(checkValidity)
+# True
+
+# Greater Than comparison of characters
+checkValidity = ('B' > 'B')
+print(checkValidity)
+# False
+
+# Greater Than comparison of characters
+checkValidity = ('B' > 'C')
+print(checkValidity)
+# False
+
+# Greater Than comparison of characters
+checkValidity = ('Z' > 'z')
+print(checkValidity)
+# False
+
+print()
+print("####################################################")
+print()
+
+# Less Than comparison of integers
+checkValidity = (1 < 0)
+print(checkValidity)
+# False
+
+# Less Than comparison of integers
+checkValidity = (1 < 1)
+print(checkValidity)
+# False
+
+# Less Than comparison of integers
+checkValidity = (1 < 2)
+print(checkValidity)
+# True
+
+# Less Than comparison of floats
+checkValidity = (1.0 < 0.0)
+print(checkValidity)
+# False
+
+# Less Than comparison of floats
+checkValidity = (1.0 < 1.0)
+print(checkValidity)
+# False
+
+# Less Than comparison of floats
+checkValidity = (1.0 < 2.0)
+print(checkValidity)
+# True
+
+# Less Than comparison of Strings
+checkValidity = ("B" < "A")
+print(checkValidity)
+# False
+
+# Less Than comparison of Strings
+checkValidity = ("B" < "B")
+print(checkValidity)
+# False
+
+# Less Than comparison of Strings
+checkValidity = ("B" < "C")
+print(checkValidity)
+# True
+
+# Less Than comparison of Strings
+checkValidity = ("Z" < "z")
+print(checkValidity)
+# True
+
+# Less Than comparison of characters
+checkValidity = ('B' < 'A')
+print(checkValidity)
+# False
+
+# Less Than comparison of characters
+checkValidity = ('B' < 'B')
+print(checkValidity)
+# False
+
+# Less Than comparison of characters
+checkValidity = ('B' < 'C')
+print(checkValidity)
+# True
+
+# Less Than comparison of characters
+checkValidity = ('Z' < 'z')
+print(checkValidity)
+# True
+
+print()
+print("####################################################")
+print()
+
+# Greater Than or Equal To comparison of integers
+checkValidity = (1 >= 0)
+print(checkValidity)
+# True
+
+# Greater Than or Equal To comparison of integers
+checkValidity = (1 >= 1)
+print(checkValidity)
+# True
+
+# Greater Than or Equal To comparison of integers
+checkValidity = (1 >= 2)
+print(checkValidity)
+# False
+
+# Greater Than or Equal To comparison of floats
+checkValidity = (1.0 >= 0.0)
+print(checkValidity)
+# True
+
+# Greater Than or Equal To comparison of floats
+checkValidity = (1.0 >= 1.0)
+print(checkValidity)
+# True
+
+# Greater Than or Equal To comparison of floats
+checkValidity = (1.0 >= 2.0)
+print(checkValidity)
+# False
+
+# Greater Than or Equal To comparison of Strings
+checkValidity = ("B" >= "A")
+print(checkValidity)
+# True
+
+# Greater Than or Equal To comparison of Strings
+checkValidity = ("B" >= "B")
+print(checkValidity)
+# True
+
+# Greater Than or Equal To comparison of Strings
+checkValidity = ("B" >= "C")
+print(checkValidity)
+# False
+
+# Greater Than or Equal To comparison of Strings
+checkValidity = ("Z" >= "z")
+print(checkValidity)
+# False
+
+# Greater Than or Equal To comparison of characters
+checkValidity = ('B' >= 'A')
+print(checkValidity)
+# True
+
+# Greater Than or Equal To comparison of characters
+checkValidity = ('B' >= 'B')
+print(checkValidity)
+# True
+
+# Greater Than or Equal To comparison of characters
+checkValidity = ('B' >= 'C')
+print(checkValidity)
+# False
+
+# Greater Than or Equal To comparison of characters
+checkValidity = ('Z' >= 'z')
+print(checkValidity)
+# False
+
+print()
+print("####################################################")
+print()
+
+# Less Than or Equal To comparison of integers
+checkValidity = (1 <= 0)
+print(checkValidity)
+# False
+
+# Less Than or Equal To comparison of integers
+checkValidity = (1 <= 1)
+print(checkValidity)
+# True
+
+# Less Than or Equal To comparison of integers
+checkValidity = (1 <= 2)
+print(checkValidity)
+# True
+
+# Less Than or Equal To comparison of floats
+checkValidity = (1.0 <= 0.0)
+print(checkValidity)
+# False
+
+# Less Than or Equal To comparison of floats
+checkValidity = (1.0 <= 1.0)
+print(checkValidity)
+# True
+
+# Less Than or Equal To comparison of floats
+checkValidity = (1.0 <= 2.0)
+print(checkValidity)
+# True
+
+# Less Than or Equal To comparison of Strings
+checkValidity = ("B" <= "A")
+print(checkValidity)
+# False
+
+# Less Than or Equal To comparison of Strings
+checkValidity = ("B" <= "B")
+print(checkValidity)
+# True
+
+# Less Than or Equal To comparison of Strings
+checkValidity = ("B" <= "C")
+print(checkValidity)
+# True
+
+# Less Than or Equal To comparison of Strings
+checkValidity = ("Z" <= "z")
+print(checkValidity)
+# True
+
+# Less Than or Equal To comparison of characters
+checkValidity = ('B' <= 'A')
+print(checkValidity)
+# False
+
+# Less Than or Equal To comparison of characters
+checkValidity = ('B' <= 'B')
+print(checkValidity)
+# True
+
+# Less Than or Equal To comparison of characters
+checkValidity = ('B' <= 'C')
+print(checkValidity)
+# True
+
+# Less Than or Equal To comparison of characters
+checkValidity = ('Z' <= 'z')
+print(checkValidity)
+# True
+
+print()
+print("####################################################")
+print()
