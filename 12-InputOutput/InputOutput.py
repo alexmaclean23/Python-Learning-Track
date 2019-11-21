756df94ed89e9cb7fdfd675cf83e3b2d63f27a8e
alexmaclean23 <alexmaclean23@gmail.com>
2019-11-20 22:13:43 -0500

    Add InputOuput content

---
+++ 12-InputOutput/InputOutput.py
@@ -0,0 +1,44 @@
+# Declaration and opening of a file
+myFile = open("Python-Learning-Track/12-InputOutput/inputFile.txt")
+
+# Reading the contents of a file as a String
+myFile.seek(0)
+print(myFile.read())
+print()
+
+# Reading the contents of a file as a List
+myFile.seek(0)
+print(myFile.readlines())
+print()
+
+# Closing file at end of use <IMPORTANT>
+myFile.close()
+
+# Using a file temporarily in a function with read permissions
+with open("Python-Learning-Track/12-InputOutput/inputFile.txt", mode = "r") as myFile:
+    fileContents = myFile.read()
+print(fileContents)
+print()
+
+# Using a file temporarily in a function with append permissions
+with open("Python-Learning-Track/12-InputOutput/inputFile.txt", mode = "a") as myFile:
+    myFile.write("\nThis is a .txt file.")
+
+# Using a file temporarily in a function with read permissions
+with open("Python-Learning-Track/12-InputOutput/inputFile.txt", mode = "r") as myFile:
+    fileContents = myFile.read()
+print(fileContents)
+print()
+
+# Using a file temporarily in a function with write permissions
+with open("Python-Learning-Track/12-InputOutput/inputFile.txt", mode = "w") as myFile:
+    myFile.write("This is a .txt file.\nThis is a .txt file.\nThis is a .txt file.")
+
+# Using a file temporarily in a function with read permissions
+with open("Python-Learning-Track/12-InputOutput/inputFile.txt", mode = "r") as myFile:
+    fileContents = myFile.read()
+print(fileContents)
+print()
+
+# The mode identifier "r+" allows a file to be read, and then written
+# The mode identifier "w+" allows a file to be written, and then read
