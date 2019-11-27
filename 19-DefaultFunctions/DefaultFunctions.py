e6ec4d60db8c573a91e21232912bec21cb0d3af7
alexmaclean23 <alexmaclean23@gmail.com>
2019-11-27 11:14:02 -0500

    Add DefaultFunctions Content

---
+++ 19-DefaultFunctions/DefaultFunctions.py
@@ -0,0 +1,27 @@
+# Demonstration of the range function
+for x in range(0, 5, 1):
+    print(x)
+print()
+
+# Demonstration of the enumerate function
+for x in enumerate("abcde"):
+    print(x)
+print()
+
+# Demonstration of the zip function
+for x in zip([1, 2, 3], ['a', 'b', 'c']):
+    print(x)
+print()
+
+# Demonstration of the in keyword
+checkValidity = 5.0 in [1, 2, 3]
+print(checkValidity)
+checkValidity = '!' in "Hello World!"
+print(checkValidity)
+print()
+
+# Demonstration of the min and max functions
+myList = [1, 2, 3, 4, 5]
+print(max(myList))
+print(max(myList))
+print()
