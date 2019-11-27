65c95ddb41c68757daa1198d3e5bc66f6013cae6
alexmaclean23 <alexmaclean23@gmail.com>
2019-11-27 01:06:11 -0500

    Add WhileLoops content

---
+++ 17-WhileLoops/WhileLoops.py
@@ -0,0 +1,9 @@
+# Declaration if an integer variable
+myNum = 0
+
+# While loops that increment and prints nyNum up to 5
+while (myNum <= 5):
+    print(f"myNum is currently {myNum}")
+    myNum += 1
+else:
+    print("myNum is no longer in range.")
